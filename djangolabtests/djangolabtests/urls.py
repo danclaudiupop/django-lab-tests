273c4310516cc58d01bf4fff90c8e2c1f2c73a8a
Dan Claudiu Pop <danclaudiupop@gmail.com>
2013-03-20 22:35:23 +0200

    direct_to_template deprecated

--- djangolabtests/djangolabtests/urls.py
+++ djangolabtests/djangolabtests/urls.py
@@ -1,4 +1,3 @@
-from django.views.generic.simple import direct_to_template
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
@@ -8,6 +7,5 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.urls')),
-    #url(r'^$', TemplateView.as_view(template_name="index.html")),
-    url(r'^$', direct_to_template, { 'template': 'index.html' },),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 )
