7b715ed73da7306233d4a420e0b02c4ad73f81a0
Dan Claudiu Pop <danclaudiupop@gmail.com>
2013-03-29 11:29:45 +0200

    functional tests

---
+++ djangolabtests/functional_tests/test_login.py
@@ -0,0 +1,47 @@
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+from django_webtest import WebTest
+from django_nose.tools import assert_equals, assert_contains
+
+
+class LoginTest(WebTest):
+
+    def setUp(self):
+        User.objects.create_user(
+            username='danu',
+            password='test123',
+        )
+
+    def testLoginProcess(self):
+        login = self.app.get(reverse('auth_login'))
+        login.form['username'] = 'danu'
+        login.form['password'] = 'test123'
+        response = login.form.submit('Log in').follow()
+        assert_equals('200 OK', response.status)
+        assert_contains(response, 'Welcome danu :]', count=1, status_code=200)
+
+    def testLoginWithInvalidCredentials(self):
+        login = self.app.get(reverse('auth_login'))
+        login.form['username'] = 'username'
+        login.form['password'] = 'password'
+        response = login.form.submit('Log in')
+        assert_contains(
+            response,
+            'Please enter a correct username and password. '
+            'Note that both fields are case-sensitive.',
+            count=1,
+            status_code=200
+        )
+
+    def testSubmitWithEmptyCredentials(self):
+        login = self.app.get(reverse('auth_login'))
+        login.form['username'] = ''
+        login.form['password'] = ''
+        response = login.form.submit('Log in')
+        assert_contains(
+            response,
+            'This field is required.',
+            count=2,
+            status_code=200
+        )
---
+++ djangolabtests/functional_tests/test_register.py
@@ -0,0 +1,98 @@
+import re
+import datetime
+
+from django import forms
+from django.core import mail
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
+
+from django_webtest import WebTest
+from django_nose.tools import assert_contains
+
+
+class RegisterTest(WebTest):
+
+    fixtures = ['users']
+
+    def testRegisterProcess(self):
+        register = self.app.get(reverse('registration_register'))
+        register.form['username'] = 'danul'
+        register.form['email'] = 'danclaudiupop@gmail.com'
+        register.form['password1'] = 'test123'
+        register.form['password2'] = 'test123'
+        response = register.form.submit('Submit').follow()
+        assert_contains(
+            response,
+            'You are now registered. Activation email sent.',
+            count=1,
+            status_code=200
+        )
+        activation_url = re.search(
+            '/accounts/activate/(.*)',
+            mail.outbox[0].body
+        ).group(0)
+        response = self.app.get(activation_url).follow()
+        assert_contains(
+            response,
+            'Your account is now activated.',
+            count=1,
+            status_code=200
+        )
+
+    def testActivationExpired(self):
+        register = self.app.get(reverse('registration_register'))
+        register.form['username'] = 'danul'
+        register.form['email'] = 'danclaudiupop@gmail.com'
+        register.form['password1'] = 'test123'
+        register.form['password2'] = 'test123'
+        response = register.form.submit('Submit').follow()
+        user = User.objects.get(username='danul')
+        user.date_joined -= datetime.timedelta(
+            days=settings.ACCOUNT_ACTIVATION_DAYS
+        )
+        user.save()
+        activation_url = re.search(
+            '/accounts/activate/(.*)',
+            mail.outbox[0].body
+        ).group(0)
+        response = self.app.get(activation_url)
+        assert_contains(
+            response,
+            'Account activation failed',
+            count=1,
+            status_code=200
+        )
+
+    def testUserAlreadyExists(self):
+        register = self.app.get(reverse('registration_register'))
+        register.form['username'] = 'danu'
+        register.form['email'] = 'example@gmail.com'
+        register.form['password1'] = 'test123'
+        register.form['password2'] = 'test123'
+        response = register.form.submit('Submit')
+        assert_contains(
+            response,
+            'A user with that username already exists.',
+            count=1,
+            status_code=200
+        )
+
+    def testEmailFieldValidation(self):
+        register = self.app.get(reverse('registration_register'))
+        register.form['username'] = 'danu'
+        register.form['email'] = 'abc'
+        register.form['password1'] = 'test123'
+        register.form['password2'] = 'test123'
+        response = register.form.submit('Submit')
+        self.assertFieldOutput(
+            forms.EmailField,
+            {'dan@dan.com': 'dan@dan.com'},
+            {'abc': [u'Enter a valid e-mail address.']}
+        )
+        assert_contains(
+            response,
+            'Enter a valid e-mail address.',
+            count=1,
+            status_code=200
+        )
